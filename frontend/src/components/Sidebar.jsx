dab2a6b4eecbfa2ca93b0aaa8764c3aedaa26f81
Kunal <rohna1236@gmail.com>
2025-04-12 12:25:43 +0530

    delete playlistcard

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { assets } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
-import PlayListCard from "./PlayListCard";
+// import PlayListCard from "./PlayListCard";
 import { UserData } from "../context/User";
 
 const Sidebar = () => {
